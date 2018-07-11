4884ef9e21426c77a8f95fa2c4d1ad8761389b82
Lucas <lucasvalenteds@gmail.com>
2018-07-11 05:15:52 -0300

    Update Gradle dependencies
    Also add plugin to check them.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.2.50"
     id("com.adarshr.test-logger") version "1.3.1"
+    id("com.github.ben-manes.versions") version "0.20.0"
 }
 
 subprojects {
     apply {
         plugin("org.jetbrains.kotlin.jvm")
+        plugin("com.adarshr.test-logger")
     }
 
     repositories {
@@ -18,11 +20,11 @@ subprojects {
 
     dependencies {
         compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-        compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.22.2")
+        compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.23.4")
 
-        testCompile("org.junit.jupiter:junit-jupiter-api:5.1.0-RC1")
-        testCompile("org.junit.jupiter:junit-jupiter-params:5.1.0-RC1")
-        testRuntime("org.junit.jupiter:junit-jupiter-engine:5.1.0-RC1")
+        testCompile("org.junit.jupiter:junit-jupiter-api:5.3.0-M1")
+        testCompile("org.junit.jupiter:junit-jupiter-params:5.3.0-M1")
+        testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.0-M1")
     }
 
     tasks {
--- firebase-tasks/build.gradle.kts
+++ firebase-tasks/build.gradle.kts
@@ -1,4 +1,3 @@
 dependencies {
     compile("com.google.firebase:firebase-admin:5.3.0")
-    compile("com.google.api:api-common:1.2.0")
 }
